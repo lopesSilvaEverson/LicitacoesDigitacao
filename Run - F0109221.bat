1318dd7fb81e24265d2ad2a09f4912126a2d79f7
lopesSilvaEverson <lopes.silva.everson@gmail.com>
2021-02-24 11:49:38 -0300

    Licitacoes Digitacao |  Update

---
+++ Run - F0109221.bat
@@ -0,0 +1,4 @@
+echo "Script to execute Licitações"
+start "" /min "C:\Users\F0109221\AppData\Local\UiPath\app-20.4.1-beta0022\UiRobot.exe" execute --file "%~dp0Main.xaml"
+
+
---
+++ Run - F0117376.bat
@@ -0,0 +1,5 @@
+echo "Script to execute Licitações"
+start "" /min "C:\Users\F0117376\AppData\Local\UiPath\app-20.4.1-beta0022\UiRobot.exe" execute --file "%~dp0Main.xaml"
+
+
+
